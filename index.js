e96baceccc41d42ee6117b4d7c7ae8f394f330bf
PYN <yxp7531540@gmail.com>
2019-05-12 23:18:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let element = document.querySelector("main")
+element.remove()
+
+var newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "is the champion"
