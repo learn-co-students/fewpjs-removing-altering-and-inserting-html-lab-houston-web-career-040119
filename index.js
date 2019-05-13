8b6f8dcc9b92eb5ae7fb3476b47a3865cb48a704
jpavioli <justin.avioli@gmail.com>
2019-05-13 12:10:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+//removes the DOM Node "main"
+document.body.querySelector('main').remove()
+
+//creates a newHeader' variable that points to node h1#victory
+var newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR NAME is the champion"
+document.body.append(newHeader)
