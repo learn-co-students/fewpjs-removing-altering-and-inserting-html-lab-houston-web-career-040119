be4bdae35f858cec645ff0cd607111242444b7b3
EC2 Default User <ec2-user@ip-172-31-33-230.us-east-2.compute.internal>
2019-05-14 02:12:40 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelectorAll('main')[0].remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+newHeader.className = "victory"
